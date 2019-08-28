33558ae88b9c5650f24d93f05eda39f83c6ee8c5
rizho123 <v-riczho@microsoft.com>
2019-08-27 23:15:05 -0700

    updating server, main layout

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js")
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on port: ", PORT)
+})
