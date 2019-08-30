3a99e7dadf3077f96dc4b536c794b23c7f39ea63
rizho123 <v-riczho@microsoft.com>
2019-08-29 23:08:16 -0700

    updated orm functions

--- config/orm.js
+++ config/orm.js
@@ -16,5 +16,56 @@ function objectToSql(object) {
     for (var key in object) {
         array.push(key + "=" + ob[key]);
     }
-}
+
+    return array.toString();
+};
+
+var orm =  {
+    all: function(tableInput, cb) {
+        var query = "SELECT * FROM" + tableInput + ";";
+        connection.query(query, function(err, results) {
+            if (err) throw err;
+
+            cb(results);
+        })
+    },
+
+    create: function(table, cols, vals, cb) {
+        var query = "INSERT INTO " + table;
+
+        query += " (";
+        query += cols.toString();
+        query += ") ";
+        query += "VALUES (";
+        query += printQuestionMarks(vals.length);
+        query += ") ";
+
+        console.log(query);
+
+        connection.query(query, vals, function(err, results) {
+            if (err) throw err;
+
+            cb(results)
+        })
+    },
+
+    update: function(table, ColumnsValues, condition, cb) {
+        var query = "UPDATE " + table;
+
+        query += " SET ";
+        query += objectToSql(ColumnsValues);
+        query += " WHERE ";
+        query += condition;
+
+        console.log(query);
+        connection.query(query, function(err, results) {
+            if (err) throw err;
+
+            cb(results);
+        })
+    }
+};
+
+module.exports = orm;
+
 
