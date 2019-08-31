b41540d869fc4d63a1d4124678576a3478b99679
rizho123 <v-riczho@microsoft.com>
2019-08-30 23:05:32 -0700

    updating db and cntrller

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP database if exists burgers_db;
+
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INT auto_increment NOT NULL,
+    burger_name VARCHAR(300) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("SomeBurger", False), ("WhatBurger", False), ("WhoBurger", False), ("HwatBurger", False), ("ThatBurger", False);
+
+SELECT * FROM burgers;
