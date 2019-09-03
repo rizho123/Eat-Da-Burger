c56c9e641b13800fa922341957ae06f950cc60b0
rizho123 <v-riczho@microsoft.com>
2019-09-02 22:43:51 -0700

    updating

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
