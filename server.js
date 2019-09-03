ac3d7db6d19007876310e793218026cced361abc
rizho123 <v-riczho@microsoft.com>
2019-09-03 01:11:29 -0700

    updating

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js")
+var bodyParser = require("body-parser")
 var path = require("path")
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.set('port', process.env.PORT || 8080);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use("/", routes);
+app.use(routes);
 
 app.listen(PORT, function() {
     console.log("Listening on port: ", PORT)
