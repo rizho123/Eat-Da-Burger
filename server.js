567a2fc81a3b20c0bb9a1182b7b9eb10c1c0f4c8
rizho123 <v-riczho@microsoft.com>
2019-09-03 00:25:16 -0700

    updating

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var routes = require("./controllers/burgers_controller.js")
 var path = require("path")
 
 var app = express();
+var PORT = process.env.PORT || 8080;
 app.set('port', process.env.PORT || 8080);
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
