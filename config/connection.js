5e7f0c1dbab7ed02ef04e02a5a432f60a8771556
rizho123 <v-riczho@microsoft.com>
2019-09-03 00:55:49 -0700

    updating

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var local_config = {
     host: "localhost",
     port: 3306,
     user: "test_user",
     password: "password",
     database: "burgers_db"
-});
+}
+
+var connection = mysql.createConnection(local_config || process.env.DATABASE_URL);
 
 connection.connect(function(err) {
     if (err) {
